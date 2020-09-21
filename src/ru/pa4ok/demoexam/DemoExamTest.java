22edbaa18112181ccd27a873333b054a4aa3f3ad
Pa4ok <n_leontiev@mail.ru>
2020-09-21 20:12:58 +0300

    21.09.20

---
+++ src/ru/pa4ok/demoexam/DemoExamTest.java
@@ -0,0 +1,50 @@
+package ru.pa4ok.demoexam;
+
+import java.util.Scanner;
+
+public class DemoExamTest
+{
+    public static void main(String[] args)
+    {
+        /*int intValue = 2;
+        Integer integer = null;
+        float floatValue = 6.435F;
+        double doubleValue = 4.5235325432532D;
+        boolean booleanValue = true;
+        String stringValue = "234234fffg";
+        if(floatValue > intValue){
+            System.out.println("true");
+        }
+        String secondString = "234234fffg";
+        if(stringValue.equals(secondString)) {
+            System.out.println("mark1");
+        }
+        System.out.println(secondString.substring(2, 3));
+        System.out.println(secondString.contains("fff"));
+        */
+
+        /*int[] intArr = new int[10];
+        System.out.println(intArr[0]);
+        String[] strings = new String[10];
+        System.out.println(strings[0]);
+
+        for(int i=0; i<intArr.length; i++) {
+            intArr[i] = i;
+        }
+
+        for(int i : intArr) {
+            System.out.printf(i + " ");
+        }*/
+
+        /*TestObject testObject = new TestObject(1, 2, 3);
+        System.out.println(testObject);
+        testObject.test();//public
+        testObject.test2();//protected
+        System.out.println(testObject.getSumm());*/
+
+        /*Scanner in = new Scanner(System.in);
+        System.out.println(5 + in.nextInt());
+        in.close();*/
+    }
+}
+
