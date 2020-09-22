5689069fb606ef3d30d6730068d20b29ceffed60
Pa4ok <n_leontiev@mail.ru>
2020-09-22 19:26:44 +0300

    22.09.20

---
+++ src/ru/pa4ok/test/NewClass.java
@@ -0,0 +1,11 @@
+package ru.pa4ok.test;
+
+import ru.pa4ok.demoexam.Main.TestStaticClass;
+
+public class NewClass
+{
+    public void test()
+    {
+        TestStaticClass test = new TestStaticClass();
+    }
+}
