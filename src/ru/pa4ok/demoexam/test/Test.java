4b09a38d4c0a93bd870867256b1fe154e84e2e49
Pa4ok <n_leontiev@mail.ru>
2020-10-02 16:43:44 +0300

    02.10.20

---
+++ src/ru/pa4ok/demoexam/test/Test.java
@@ -0,0 +1,12 @@
+package ru.pa4ok.demoexam.test;
+
+import ru.pa4ok.demoexam.Main;
+
+public class Test
+{
+    public void test()
+    {
+        Main main = new Main();
+        //NewMain newMain = new NewMain();
+    }
+}
