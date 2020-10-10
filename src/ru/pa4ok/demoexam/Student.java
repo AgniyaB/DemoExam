0e167514661b0357f498bf52059adb16d0f20884
Pa4ok <n_leontiev@mail.ru>
2020-10-10 15:03:58 +0300

    10.10.20-2

---
+++ src/ru/pa4ok/demoexam/Student.java
@@ -0,0 +1,36 @@
+package ru.pa4ok.demoexam;
+
+public class Student
+{
+    private String name;
+    private int level;
+
+    public Student(String name, int level) {
+        this.name = name;
+        this.level = level;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", level=" + level +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
