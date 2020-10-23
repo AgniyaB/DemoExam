4261c87075ff6f8cf315d83801509390e3dd0695
Pa4ok <n_leontiev@mail.ru>
2020-10-23 12:34:36 +0300

    23.10.20-3

--- src/ru/pa4ok/demoexam/Main.java
+++ src/ru/pa4ok/demoexam/Main.java
@@ -1,62 +1,49 @@
 package ru.pa4ok.demoexam;
 
-import java.util.Arrays;
 import java.util.Random;
 
 public class Main
 {
-    /*
-    abstract Sortinger
-    - abstract void sort(int[] arr)
-    - long sortWithTime(int[] arr)
-        сохраняет в переменную текущее время
-        вызывает функцию sort()
-        передает туда массив
-        считает и возвращает время
-        потраченное на сортировку
-
-    2-3 класса сортировщиков extends Sortinger
-    реализуют функцию sort(int[] arr)
-    используя разные алгоритсы сортировки
-    аля BubbleSortinger exntends Sortinger
-    ...
-
-    алгоритмы сортивки
-    https://proglib.io/p/java-sorting-algorithms
-     */
-
     public static void main(String[] args)
     {
-        /*
-        //текущее время в миликундах
-        long timeMills = System.currentTimeMillis();
-
-        //рандомное число
         Random rand = new Random();
-        rand.nextInt(10000); //0-9999
-        */
 
-        int[] arr1 = new int[100000];
-        int[] arr2 = new int[100000];
-
-
-        Random rand = new Random();
-        for(int i=0; i<arr1.length; i++) {
-            arr1[i] = rand.nextInt(1000);
+        long startMills1 = System.currentTimeMillis();
+        String s = "rffweewfwe";
+        for(int i=1; i<10000; i++) {
+            s += i;
+            s += ',';
+            s += rand.nextInt(i);
+            s += "|";
         }
-        for(int i=0; i<arr2.length; i++) {
-            arr2[i] = rand.nextInt(1000);
+        System.out.println((System.currentTimeMillis() - startMills1) + " " + s.length());
+
+        long startMills2 = System.currentTimeMillis();
+        StringBuilder sb = new StringBuilder("rffweewfwe");
+        for(int i=1; i<10000; i++) {
+            sb.append(i)
+                    .append(',')
+                    .append(rand.nextInt(i))
+                    .append("|");
         }
+        String newS = sb.toString();
+        System.out.println((System.currentTimeMillis() - startMills2) + " " + newS.length());
+    }
+}
 
-        BubbleSortinger bubbleSortinger = new BubbleSortinger();
-        System.out.println(bubbleSortinger.sortWithTime(arr1));
+class Test
+{
+    private int i;
+    private String s;
+    private boolean b;
 
-        QiuckSortinger qiuckSortinger = new QiuckSortinger();
-        System.out.println(qiuckSortinger.sortWithTime(arr2));
-
-        //System.out.println();
-        //System.out.println(Arrays.toString(arr1));
-        //System.out.println(Arrays.toString(arr2));
-
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Test{");
+        sb.append("i=").append(i);
+        sb.append(", s='").append(s).append('\'');
+        sb.append(", b=").append(b);
+        sb.append('}');
+        return sb.toString();
     }
 }
