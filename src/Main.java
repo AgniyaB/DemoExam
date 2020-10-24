cba438f565a6ea7584e7230fecf7932cf202b0ae
Pa4ok <n_leontiev@mail.ru>
2020-10-24 14:45:50 +0300

    24.10.20-1

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,60 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main
 {
+    /*
+    abstract Sortinger
+    - abstract void sort(int[] arr)
+    - long sortWithTime(int[] arr)
+        сохраняет время в переменную
+        вызывает функцию сортировки
+        считает и возвращает время
+        затраченное насортировку
+
+    3 класса extends Sortinger
+    (BubbleSoringer extends Sortinger...)
+    каждый класс реализует разный алгоритм сортировки
+
+    тут можно подглядеть алгоритмы
+    https://proglib.io/p/java-sorting-algorithms
+     */
+
     public static void main(String[] args)
     {
-        Human human = new Human("vasya", 15, GenderEnum.MALE);
+/*
+        //текущее время в милисекундах
+        long timeMills = System.currentTimeMillis();
 
-        //можно сравнивать через ==
-        //System.out.println(human.getGender() == GenderEnum.MALE);
+        //рандом
+        Random rand = new Random();
+        rand.nextInt(); //любое число
+        rand.nextInt(10000); //0-9999
+*/
+        Random rand = new Random();
 
-        //перебор всех значений
-        /*for(GenderEnum g : GenderEnum.values()) {
-            System.out.printf(g + " ");
+        int[] arr1 = new int[50000];
+        int[] arr2 = new int[50000];
+
+        for(int i=0; i<arr1.length; i++) {
+            arr1[i] = rand.nextInt(10000);
+            arr2[i] = arr1[i];
         }
-        System.out.println();*/
 
-        //получение из строки
-        /*String s = "FEMALE";
-        GenderEnum genderEnum = GenderEnum.valueOf(s);
-        System.out.println(genderEnum);*/
+        //System.out.println(Arrays.toString(arr1));
+        //System.out.println(Arrays.toString(arr2));
+        //System.out.println();
 
-        //использование через switch
-        /*switch(human.getGender())
-        {
-            case FEMALE:
-                System.out.println(1);
-                break;
-            case MALE:
-                System.out.println(2);
-                break;
-        }*/
+        BubbleSortinger bubbleSortinger = new BubbleSortinger();
+        QuickSortinger quickSortinger = new QuickSortinger();
 
-        System.out.println(human);
+        long l1 = bubbleSortinger.sortWithTime(arr1);
+        long l2 = quickSortinger.sortWithTime(arr2);
+
+        /*System.out.println(l1 + " | " + Arrays.toString(arr1));
+        System.out.println(l2 + " | " + Arrays.toString(arr1));*/
+
+        System.out.println(l1);
+        System.out.println(l2);
     }
 }
