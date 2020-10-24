f4c2e0c2e4e2c895d1180e4c09fb778034c6ba59
Pa4ok <n_leontiev@mail.ru>
2020-10-24 14:45:24 +0300

    24.10.20-1

---
+++ src/Sortinger.java
@@ -0,0 +1,11 @@
+public abstract class Sortinger
+{
+    public abstract void sort(int[] arr);
+
+    public long sortWithTime(int[] arr)
+    {
+        long startMills = System.currentTimeMillis();
+        sort(arr);
+        return System.currentTimeMillis() - startMills;
+    }
+}
