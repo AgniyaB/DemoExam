9a3b8a7d1d4437cbb4d05e191a0ff959e6ee1a36
Pa4ok <n_leontiev@mail.ru>
2020-10-29 15:59:48 +0300

    29.10.20-1

--- src/Main.java
+++ src/Main.java
@@ -1,109 +1,108 @@
-import java.util.*;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Executable;
 
 public class Main
 {
     /*
-    abstract Sortinger<T extends Comparable>
-    - abstract void sort(T[] arr)
-    - void sortWithTime(T[] arr)
-        //сохраняет время
-        //вызывает метод sort(T[] arr)
-        //вычисляет и выводит время
-        //затраченное на сортировку
+    Exception
+    обязательны для обработки
 
-    2-3 класса extends Sortinger
-    и реализуют различные алгоритмы сортировки
-    BubbleSortinger<T extends Comparable> extends Sortinger<T>
-    ...
-    ...
+    RuntimeException extends Exception
+    не обязательны для обработки
+     */
+
+    /*
+    Book
+    - int id (>0)
+    - String title (length > 3 & <20)
+    - String author (length > 3 & <20)
+
+    BookReadException extends Exception
+
+    Library
+    - String title
+    - List<Book> books
+    - public void readBook()
+        запрашивает из консоли чтение книги
+        проверяет поля на корректность
+        если поля для книги введены некоретно
+        выбрасывает ошибку BookReadException
+        если верно, то добавляет книгу
+
+    создать библиотеку
+    и в цикле прочитать несколько книг
      */
 
     public static void main(String[] args)
     {
-        //самое яркое применение генерика
-        //List<String> list = new ArrayList<>();
+        /*Integer.parseInt("1e0");
 
-        //Container<String> container = new Container<>("lhgwroreg");
-        //System.out.println(container);
+        try {
+            Integer.parseInt("1e0");
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }*/
 
-        //Container<int> //так нельзя
-        //Container<Integer>
 
-        //test2("ewfwfewfe");
-        //test2(3454);
+        /*try {
+            FileReader fr = new FileReader("text.txt");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }*/
 
-       /* List<Book> books = new ArrayList<>();
-        for(int i=0; i<5; i++) {
-            books.add(new Book());
+        /*try {
+            FileReader fr = new FileReader("text.txt");
+            Integer.parseInt("1e0");
+
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
+        }*/
+
+        /*try {
+            FileReader fr = new FileReader("text.txt");
+            Integer.parseInt("1e0");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } finally {
+            //вызывается всегда
+            //для справки, юзать его не будем
+        }*/
+
+        try {
+            testMyException();
+        } catch (MyException e) {
+            e.printStackTrace();
         }
-        System.out.println(books);
 
-        Collections.shuffle(books);
-        System.out.println(books);*/
-
-        //вариант 1
-        /*Collections.sort(books, new Comparator<Book>() {
-            //-1 o1<o2
-            //0 01==2
-            //1 o1>o2
-            @Override
-            public int compare(Book o1, Book o2) {
-                return Integer.compare(o2.id, o1.id);
+        try {
+            //...
+            if(true) {
+                throw new MyRuntimeException("34324234");
             }
-        });
-        System.out.println(books);*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        //вариант 2 + см. интерфейс Comparable в Book
-        /*Collections.sort(books);
-        System.out.println(books);*/
-
-        Integer[] arr = {234, 4, 645, 43, 32, 5, 3, -1, 20, 30, 3, 1, 0, 34};
-        sortArr(arr);
-        System.out.println(Arrays.toString(arr));
-
-        //даже нельзя вызвать метод не подходядий под обобщение
-        //Container<Integer>[] arr1 = new Container[10];
-        //sortArr(arr1);
-
-        //текущее время в милискундах
-        long timeMills = System.currentTimeMillis();
-
-        //рандомное число
-        Random rand = new Random();
-        int i = rand.nextInt(1000);;
-
+        //testMyRuntimeException();
     }
 
-    /*private static <T> void test1(T t)
+    public static void testMyException() throws MyException {
+        throw new MyException();
+    }
+
+    public static void testMyRuntimeException()
     {
+        throw new MyRuntimeException();
+    }
 
-    }*/
-
-    /*private static <T> T test2(T t)
-    {
-        if(t instanceof String) {
-            String s = (String)t;
-            System.out.println(s.toUpperCase());
-        } else {
-            System.out.println(t);
-        }
-
-        return t;
-    }*/
-
-    private static <T extends Comparable> void sortArr(T[] arr)
-    {
-        for(int i=0; i<arr.length; i++)
-        {
-            for(int j=i; j<arr.length; j++)
-            {
-                if(arr[i].compareTo(arr[j]) == 1)
-                {
-                    T t = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = t;
-                }
-            }
+    public static void test1(int i) throws MyException {
+        if(i < 0)  {
+            throw new MyException("меньше 0");
         }
     }
+
 }
