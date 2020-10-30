ef85f1c3cb89ad9f5d0c7d8b5a862e6ac303320c
Pa4ok <n_leontiev@mail.ru>
2020-10-30 14:40:41 +0300

    30.10.20-1

--- src/ru/pa4ok/demoexam/type/IDefectiveOperational.java
+++
@@ -1,7 +0,0 @@
-package ru.pa4ok.demoexam.type;
-
-public interface IDefectiveOperational
-{
-    public boolean canProcess(double value1, double value2);
-}
-
--- src/ru/pa4ok/demoexam/type/IOperational.java
+++
@@ -1,8 +0,0 @@
-package ru.pa4ok.demoexam.type;
-
-public interface IOperational
-{
-    public double process(double value1, double value2);
-
-    public OperationTypeEnum getType();
-}
