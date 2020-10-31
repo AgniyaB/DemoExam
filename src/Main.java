9c26628b4cca268bc72fc5afc5924a430999491a
Pa4ok <n_leontiev@mail.ru>
2020-10-31 11:00:43 +0300

    31.10.20

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,9 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Executable;
+import java.util.Scanner;
 
-public class Main
-{
-    /*
-    Exception
-    обязательны для обработки
-
-    RuntimeException extends Exception
-    не обязательны для обработки
-     */
-
+public class Main {
     /*
     Book
     - int id (>0)
@@ -37,72 +29,14 @@ public class Main
 
     public static void main(String[] args)
     {
-        /*Integer.parseInt("1e0");
+        Library lib = new Library("назвнание4зшукпмркуп");
 
-        try {
-            Integer.parseInt("1e0");
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }*/
-
-
-        /*try {
-            FileReader fr = new FileReader("text.txt");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }*/
-
-        /*try {
-            FileReader fr = new FileReader("text.txt");
-            Integer.parseInt("1e0");
-
-        } catch (IOException | NumberFormatException e) {
-            e.printStackTrace();
-        }*/
-
-        /*try {
-            FileReader fr = new FileReader("text.txt");
-            Integer.parseInt("1e0");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        } finally {
-            //вызывается всегда
-            //для справки, юзать его не будем
-        }*/
-
-        try {
-            testMyException();
-        } catch (MyException e) {
-            e.printStackTrace();
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0; i<2; i++) {
+            lib.readBook(scanner);
         }
+        scanner.close();
 
-        try {
-            //...
-            if(true) {
-                throw new MyRuntimeException("34324234");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        //testMyRuntimeException();
+        System.out.println(lib);
     }
-
-    public static void testMyException() throws MyException {
-        throw new MyException();
-    }
-
-    public static void testMyRuntimeException()
-    {
-        throw new MyRuntimeException();
-    }
-
-    public static void test1(int i) throws MyException {
-        if(i < 0)  {
-            throw new MyException("меньше 0");
-        }
-    }
-
 }
