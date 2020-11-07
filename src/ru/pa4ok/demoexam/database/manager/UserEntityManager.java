40fab769325cd7d6cb59717e02312b3a127af54d
Pa4ok <n_leontiev@mail.ru>
2020-11-07 14:39:20 +0300

    07.11.20-1

---
+++ src/ru/pa4ok/demoexam/database/manager/UserEntityManager.java
@@ -0,0 +1,105 @@
+package ru.pa4ok.demoexam.database.manager;
+
+import ru.pa4ok.demoexam.database.entity.UserEntity;
+import ru.pa4ok.demoexam.util.MysqlDatabase;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserEntityManager
+{
+    private MysqlDatabase database;
+
+    public UserEntityManager(MysqlDatabase database) {
+        this.database = database;
+    }
+
+    public UserEntity add(UserEntity userEntity) throws SQLException
+    {
+        try(Connection c = database.getConnection())
+        {
+            String sql = "INSERT INTO users (login, password) values (?,?)";
+            PreparedStatement s = c.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            s.setString(1, userEntity.getLogin());
+            s.setString(2, userEntity.getPassword());
+            s.executeUpdate();
+
+            ResultSet resultSet = s.getGeneratedKeys();
+            if (resultSet.next()) {
+                userEntity.setId(resultSet.getInt(1));
+                return userEntity;
+            }
+
+            throw new SQLException("User not addeds");
+        }
+    }
+
+    public UserEntity getById(int id) throws SQLException
+    {
+        try(Connection c = database.getConnection())
+        {
+            String sql = "SELECT * FROM users WHERE id=?";
+            PreparedStatement s = c.prepareStatement(sql);
+            s.setInt(1, id);
+
+            ResultSet resultSet = s.executeQuery();
+            if(resultSet.next()) {
+                return new UserEntity(
+                        resultSet.getInt("id"),
+                        resultSet.getString("login"),
+                        resultSet.getString("PASSWORD")
+                );
+            }
+
+            return null;
+        }
+    }
+
+    public List<UserEntity> getAll() throws SQLException
+    {
+        try(Connection c = database.getConnection())
+        {
+            String sql = "SELECT * FROM users";
+            Statement s = c.createStatement();
+            ResultSet resultSet = s.executeQuery(sql);
+
+            List<UserEntity> users = new ArrayList<>();
+            while(resultSet.next()) {
+                users.add(new UserEntity(
+                        resultSet.getInt("id"),
+                        resultSet.getString("login"),
+                        resultSet.getString("PASSWORD")
+                ));
+            }
+
+            return users;
+        }
+    }
+
+    public int update(UserEntity userEntity) throws SQLException
+    {
+        try(Connection c = database.getConnection())
+        {
+            String sql = "UPDATE users SET login=?, PASSWORD=? WHERE id=?";
+            PreparedStatement s = c.prepareStatement(sql);
+            s.setString(1, userEntity.getLogin());
+            s.setString(2, userEntity.getPassword());
+            s.setInt(3, userEntity.getId());
+
+            return s.executeUpdate();
+        }
+    }
+
+    public int deleteById(int id) throws SQLException
+    {
+        try(Connection c = database.getConnection())
+        {
+            String sql = "DELETE FROM users WHERE id=?";
+            PreparedStatement s = c.prepareStatement(sql);
+            s.setInt(1, id);
+
+            return s.executeUpdate();
+        }
+    }
+}
---
+++ src/ru/pa4ok/demoexam/util/MysqlDatabase.java
@@ -0,0 +1,71 @@
+package ru.pa4ok.demoexam.util;
+
+import com.mysql.cj.jdbc.MysqlDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class MysqlDatabase
+{
+    private final String address;
+    private final int port;
+    private final String name;
+    private final String user;
+    private final String pass;
+
+    private DataSource source;
+
+    public MysqlDatabase(String address, int port, String name, String user, String pass)
+    {
+        this.address = address;
+        this.port = port;
+        this.name = name;
+        this.user = user;
+        this.pass = pass;
+    }
+
+    public MysqlDatabase(String address, String name, String user, String pass) {
+        this(address, 3306, name, user, pass);
+    }
+
+    public Connection getConnection() throws SQLException
+    {
+        if(source == null) {
+            MysqlDataSource mysqlDataSource = new MysqlDataSource();
+
+            mysqlDataSource.setServerName(address);
+            mysqlDataSource.setPort(port);
+            mysqlDataSource.setDatabaseName(name);
+            mysqlDataSource.setUser(user);
+            mysqlDataSource.setPassword(pass);
+
+            mysqlDataSource.setCharacterEncoding("UTF-8");
+            mysqlDataSource.setServerTimezone("UTC");
+
+            this.source = mysqlDataSource;
+        }
+
+        return this.source.getConnection();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+}
