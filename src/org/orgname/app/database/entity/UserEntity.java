7231e74e633b9c7a21beafbc945d2c957c6eca3b
Pa4ok <n_leontiev@mail.ru>
2020-11-14 14:53:34 +0300

    14.11.20

--- src/org/orgname/app/database/entity/UserEntity.java
+++ src/org/orgname/app/database/entity/UserEntity.java
@@ -5,15 +5,41 @@ public class UserEntity
     private int id;
     private String login;
     private String password;
+    private int age;
+    private String job;
 
-    public UserEntity(int id, String login, String password) {
+    public UserEntity(int id, String login, String password, int age, String job) {
         this.id = id;
         this.login = login;
         this.password = password;
+        this.age = age;
+        this.job = job;
     }
 
-    public UserEntity(String login, String password) {
-        this(-1, login, password);
+    public UserEntity(String login, String password, int age, String job) {
+        this(-1, login, password, age, job);
+    }
+
+    public static boolean isLoginInCorrect(String login) {
+        return login.length() > 20;
+    }
+
+    public static boolean isPasswordInCorrect(String pass) {
+        return pass.length() > 32;
+    }
+
+    public static boolean isAgeInCorrect(String ageString) {
+        try {
+            int age = Integer.parseInt(ageString);
+            return age <= 0;
+        } catch (Exception e) {
+            return true;
+        }
+    }
+
+    public static boolean isJobInCorrect(String job)
+    {
+        return job.length() > 45;
     }
 
     @Override
@@ -22,6 +48,8 @@ public class UserEntity
                 "id=" + id +
                 ", login='" + login + '\'' +
                 ", password='" + password + '\'' +
+                ", age=" + age +
+                ", job='" + job + '\'' +
                 '}';
     }
 
@@ -47,5 +75,21 @@ public class UserEntity
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
     }
 }
