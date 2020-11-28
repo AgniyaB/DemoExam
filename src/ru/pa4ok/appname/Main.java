2aaeec5102e535566f91349b0edc5b6ca2dee00a
Pa4ok <n_leontiev@mail.ru>
2020-11-28 15:46:02 +0300

    28.11.20

--- src/ru/pa4ok/appname/Main.java
+++ src/ru/pa4ok/appname/Main.java
@@ -1,120 +1,59 @@
 package ru.pa4ok.appname;
 
-import ru.pa4ok.test.Note;
+import java.util.Arrays;
 
 public class Main
 {
     /*
-    быстрое продолжение
-        psvm - главнвый метод main
-        sout - принтл с новой строкой
-        souf - принт без новой строки
-
-    комбинации
-    alt + insert - генерация конструкторов, toString, геттеров и сеттеров
-    alt + enter - предложение вариантов по исправлению ошибок
-    если ноут и insert на 2 действии кнопки то alt + fn + insert
-     */
-
-    /*
-    Person
-    - int id
-    - String firstname
+    Student
     - String surname
-    - int age
+    - String firstname
+    - int level //класс
 
-    должен быть переопределен метод toString()
-    все поля private + геттеры и сеттеры
+    Teacher
+    - String surname
+    - String firstname
+    - int exp //стаж
 
-    сделать массив из Person
-    и в цикле его заполнить
-    пример: new Person(i, "firstname-" + i, "surname-" + i....)
-    вывести массив в консоль
+    School
+    - int number
+    - String title
+    - Teacher teacher
+    - Student student
+    
+    у всех классов переопределен метод toString()
+    все поля приватные + геттеры и сеттеры
+    создать объект школы с студертом и учителем
+    вывести его в консоль
      */
 
     public static void main(String[] args)
     {
-        /*//System.out.println("Hello World " + 228);
-
-        //у примитивов есть дефолтные значения
-        int i = 4;
-        char c = '~';
-        boolean booleanValue = false;
-        float f = 3.5445F;
-        double d = 3.432432D;
-
-        String s; //по умолчанию null
-        String s1 = "";
-        String s2 = "iueFIEfgiugf23";
-        //в строках сразу вложены все методы
-        System.out.println(s2.substring(3, 6));
-
-        int[] intArr; //сейчас массив null
-
-        int[] intArr1 = new int[10];
-        System.out.println(intArr1[0]); //0
-
-        String[] strArr = new String[10];
-        System.out.println(strArr[0]); //null
-        strArr[0] = "test1234";
-        System.out.println(strArr[0]);
-
-        //все примитивы имееют свои глобальные классы
-        //по умолчанию как и любые сложные типы данных они равны null
-
-        int i2; //0
-        Integer integer; //null
-
-        Character character;
-        Boolean bool;
-        Float floatGlobal;
-        Double doubleGlobal;
-
-        String intString = "343434";
-        int i3 = Integer.parseInt(intString);
-        i3 += 10000000;
-        System.out.println("result" + i3);
-
-        Boolean.parseBoolean("false");
-        Float.parseFloat("4.543434343");
-        Double.parseDouble("3.324348");*/
-
-        int[] arr = new int[10];
-        /*for(int i=0; i<arr.length; i++) {
-            System.out.printf(arr[i] + " ");
-        }
-        System.out.println();
-        for(int i=0; i<arr.length; i++) {
-            arr[i] = 9-i;
-        }
-        for(int i=0; i<arr.length; i++) {
-            System.out.printf(arr[i] + " ");
-        }
-        System.out.println();*/
-        /*System.out.println(Arrays.toString(arr));
-        for(int i=0; i<arr.length; i++) {
-            arr[i] = 9-i;
-        }
+        /*Person[] arr = new Person[10];
         System.out.println(Arrays.toString(arr));
-
-        int temp = 0;
-        for(int i : arr) {
-            temp += i;
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = new Person(1, "first-" + i, "surname-" + i, 15 + i);
         }
-        System.out.println(temp);*/
+        System.out.println(Arrays.toString(arr));*/
 
-        /*while(true) {
-            //...
-        }
-        for(;;) {
-            //...
-        }*/
+        /*Passport passport = new Passport(
+                4014,
+                077754,
+                "spb peso4naya 14"
+        );*/
 
-        /*Book book = new Book(1, "vozna i mir", "pushkin");
-        //System.out.println(book);
-        System.out.println(book.author);*/
+        Person p = new Person(
+                1,
+                "Vasya",
+                "Shurik",
+                15,
+                new Passport(
+                        4014,
+                        077754,
+                        "spb peso4naya 14"
+                )
+        );
 
-        Note note = new Note(0, "olehfewbfew4543ogho34ht");
-        System.out.println(note.getText());
+        System.out.println(p.getPassport().getAddress());
     }
 }
