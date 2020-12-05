7c90c89636c5a317e2acb7cc6cf051aa82a45622
Pa4ok <n_leontiev@mail.ru>
2020-12-05 15:53:05 +0300

    05.12.20-3

--- src/ru/pa4ok/appname/Main.java
+++ src/ru/pa4ok/appname/Main.java
@@ -31,87 +31,16 @@ public class Main
 
     public static void main(String[] args)
     {
-        //List<int> нельзя, List<Integer> можно
-        List<String> list = new ArrayList<>();
+        School school = new School(1, "первая школа кибербулинга");
 
-        String s = "second";
+        school.getTeachers().add(new Teacher("кибербулер", "228","кибербуллинг", 10));
+        school.getTeachers().add(new Teacher("кибербулер", "228","кибербуллинг", 10));
+        school.getTeachers().add(new Teacher("кибербулер", "228","кибербуллинг", 10));
 
-        list.add("first");
-        list.add(s);
-        list.add("third");
+        school.getStudents().add(new Student("vasya", "kolpin", 15));
+        school.getStudents().add(new Student("vasya", "kolpin", 15));
+        school.getStudents().add(new Student("vasya", "kolpin", 15));
 
-        //System.out.println(list.get(1));
-
-        /*System.out.println(list);
-        list.remove(s); //удаление по объекту
-        System.out.println(list);
-        list.remove(1); //удаление по индексу
-        System.out.println(list);*/
-
-        /*int size = list.size();
-        String first = list.get(0);
-        boolean isEmpty = list.isEmpty();
-        list.clear();*/
-
-        //System.out.println(list.contains(s));
-        //System.out.println(list.contains("eiofhowefh"));
-
-
-
-
-        /*TestClass test = new TestClass();
-        test.getStrings().add("eoffewfewjofe");
-        test.getStrings().add("ijfofejwfqew");
-        System.out.println(test);
-        */
-
-        /*TestClass test = new TestClass(list);
-        System.out.println(test);
-        test.getStrings().clear();
-        System.out.println(test);*/
-
-        /*
-        Если у Вас стоит java >= 9 тогда спокойно используйте List.of("woidhwd", "oewhfewf")
-
-        Если стоит 8 java есть Arrays.asList("woidhwd", "oewhfewf")
-        но есть ограничение, данная функция возвращает список который нельзя изменять
-        это можно обойти через пересоздание списка
-        new ArrayList<>(Arrays.asList("ofjfei", "oewfjoifew", "oefefwew"))
-         */
-        TestClass test = new TestClass(
-                new ArrayList<>(Arrays.asList("ofjfei", "oewfjoifew", "oefefwew"))
-        );
-        System.out.println(test);
-        test.getStrings().add("eofkewffe");
-        System.out.println(test);
-    }
-}
-
-class TestClass
-{
-    //private List<String> strings = new ArrayList<>();
-    private List<String> strings;
-
-    public TestClass(List<String> strings) {
-        this.strings = strings;
-    }
-
-    public TestClass() {
-        this(new ArrayList<>());
-    }
-
-    @Override
-    public String toString() {
-        return "TestClass{" +
-                "strings=" + strings +
-                '}';
-    }
-
-    public List<String> getStrings() {
-        return strings;
-    }
-
-    public void setStrings(List<String> strings) {
-        this.strings = strings;
+        System.out.println(school);
     }
 }
---
+++ src/ru/pa4ok/appname/School.java
@@ -0,0 +1,59 @@
+package ru.pa4ok.appname;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School
+{
+    private int index;
+    private String title;
+    private List<Teacher> teachers = new ArrayList<>();
+    private List<Student> students = new ArrayList<>();
+
+    public School(int index, String title) {
+        this.index = index;
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "index=" + index +
+                ", title='" + title + '\'' +
+                ", teachers=" + teachers +
+                ", students=" + students +
+                '}';
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(List<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+}
