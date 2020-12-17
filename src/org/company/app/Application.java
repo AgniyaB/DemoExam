86a1475923c3313ec7f3b06fb60676a7e371997d
Pa4ok <n_leontiev@mail.ru>
2020-12-17 16:36:38 +0300

    17.12.20-2

--- src/org/company/app/Application.java
+++ src/org/company/app/Application.java
@@ -1,5 +1,7 @@
 package org.company.app;
 
+import org.company.app.data.entity.DateEntity;
+import org.company.app.data.manager.DateEntityManager;
 import org.company.app.data.manager.UserEntityManager;
 import org.company.app.ui.service.ServiceTableForm;
 import org.company.app.ui.user.CustomTableForm;
@@ -10,13 +12,16 @@ import org.company.app.util.MysqlDatabase;
 import javax.swing.*;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Application
 {
     private static Application instance;
 
-    //private final MysqlDatabase database = new MysqlDatabase("116.202.236.174", "DemoExam", "DemoExam", "DemoExam");
-    private final MysqlDatabase database = new MysqlDatabase("nleontnr.beget.tech", "nleontnr_docker", "nleontnr_docker", "8udwX&9bdw");
+    private final MysqlDatabase database = new MysqlDatabase("116.202.236.174", "DemoExam", "DemoExam", "DemoExam");
+    //private final MysqlDatabase database = new MysqlDatabase("nleontnr.beget.tech", "nleontnr_docker", "nleontnr_docker", "8udwX&9bdw");
 
     private final UserEntityManager userEntityManager = new UserEntityManager(database);
 
@@ -27,7 +32,18 @@ public class Application
         initDatabase();
         initUi();
 
-        new ServiceTableForm();
+        String birthday = "18.07.2000";
+        SimpleDateFormat format3 = new SimpleDateFormat("dd.MM.yyyy");
+        try {
+            DateEntityManager dateEntityManager = new DateEntityManager(database);
+            DateEntity dateEntity = new DateEntity(format3.parse(birthday));
+            dateEntityManager.add(dateEntity);
+            System.out.println(dateEntity);
+        } catch (ParseException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        //new ServiceTableForm();
         //new StartForm();
     }
 
