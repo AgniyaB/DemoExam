cb85b50a39138b93bc17d3253c1d4a3297f5c1b2
Pa4ok <n_leontiev@mail.ru>
2020-12-19 12:20:31 +0300

    19.12.20-2

--- src/org/orgname/app/ui/ServiceTableForm.java
+++ src/org/orgname/app/ui/ServiceTableForm.java
@@ -5,13 +5,18 @@ import org.orgname.app.database.entity.ServiceEntity;
 import org.orgname.app.database.manager.ServiceEntityManager;
 import org.orgname.app.util.BaseForm;
 import org.orgname.app.util.CustomTableModel;
+import org.orgname.app.util.DialogUtil;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
 import java.awt.*;
+import java.awt.event.ItemEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.sql.SQLException;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.List;
 
 public class ServiceTableForm extends BaseForm
 {
@@ -20,12 +25,15 @@ public class ServiceTableForm extends BaseForm
     private CustomTableModel<ServiceEntity> model;
 
     private boolean discountSort = false;
+    private int maxRowCount = 0;
 
     private JPanel mainPanel;
     private JTable table;
     private JButton discountSortButton;
     private JComboBox discountSortBox;
     private JButton clearButton;
+    private JLabel valuesCountLabel;
+    private JButton addButton;
 
     public ServiceTableForm()
     {
@@ -49,6 +57,8 @@ public class ServiceTableForm extends BaseForm
                     serviceEntityManager.getAll()
             );
             table.setModel(model);
+            maxRowCount = model.getValues().size();
+            updateRowCount();
 
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -64,15 +74,72 @@ public class ServiceTableForm extends BaseForm
                 return c;
             }
         });
+
+        if(Application.isAdminMode()) {
+            table.addKeyListener(new KeyAdapter() {
+                @Override
+                public void keyPressed(KeyEvent e) {
+                    int row = table.getSelectedRow();
+                    if(e.getKeyCode() == KeyEvent.VK_DELETE && row != -1) {
+                        if(DialogUtil.showConfirm(ServiceTableForm.this, "Вы точно хотите удалить данную запись?")) {
+                            try {
+                                serviceEntityManager.delete(model.getValues().get(row));
+                                model.getValues().remove(row);
+                                model.fireTableDataChanged();
+
+                            } catch (SQLException throwables) {
+                                throwables.printStackTrace();
+                            }
+                        }
+                    }
+                }
+            });
+        }
     }
 
     private void initBoxes()
     {
+        discountSortBox.addItem("Все");
         discountSortBox.addItem("от 0 до 5%");
         discountSortBox.addItem("от 5% до 15%");
         discountSortBox.addItem("от 15% до 30%");
         discountSortBox.addItem("от 30% до 70%");
         discountSortBox.addItem("от 70% до 100%");
+
+        discountSortBox.addItemListener(e -> {
+            if(e.getStateChange() == ItemEvent.SELECTED) {
+                try {
+                    List<ServiceEntity> services = serviceEntityManager.getAll();
+                    maxRowCount = services.size();
+                    switch (discountSortBox.getSelectedIndex())
+                    {
+                        case 0:
+                            break;
+                        case 1:
+                            services.removeIf(serviceEntity -> serviceEntity.getDiscount() >= 5);
+                            break;
+                        case 2:
+                            services.removeIf(serviceEntity -> serviceEntity.getDiscount() < 5 || serviceEntity.getDiscount() >= 15);
+                            break;
+                        case 3:
+                            services.removeIf(serviceEntity -> serviceEntity.getDiscount() < 15 || serviceEntity.getDiscount() >= 30);
+                            break;
+                        case 4:
+                            services.removeIf(serviceEntity -> serviceEntity.getDiscount() < 30 || serviceEntity.getDiscount() >= 70);
+                            break;
+                        case 5:
+                            services.removeIf(serviceEntity -> serviceEntity.getDiscount() < 70);
+                            break;
+                    }
+                    model.setValues(services);
+                    model.fireTableDataChanged();
+                    updateRowCount();
+
+                } catch (SQLException throwables) {
+                    throwables.printStackTrace();
+                }
+            }
+        });
     }
 
     private void initButtons()
@@ -93,14 +160,18 @@ public class ServiceTableForm extends BaseForm
         });
 
         clearButton.addActionListener(e -> {
-            try {
-                model.setValues(serviceEntityManager.getAll());
-                model.fireTableDataChanged();
-
-            } catch (SQLException throwables) {
-                throwables.printStackTrace();
-            }
+            discountSort = false;
+            discountSortBox.setSelectedIndex(0);
         });
+
+        if(!Application.isAdminMode()) {
+            addButton.setVisible(false);
+        }
+    }
+
+    private void updateRowCount()
+    {
+        valuesCountLabel.setText("Показано записей " + model.getValues().size() + "/" + maxRowCount);
     }
 
     @Override
