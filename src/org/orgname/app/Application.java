c40e3e247db17bc4081f100c2ae4ecb824f0d338
Pa4ok <n_leontiev@mail.ru>
2020-12-23 19:15:14 +0300

    23.12.20

--- src/org/orgname/app/Application.java
+++ src/org/orgname/app/Application.java
@@ -1,5 +1,7 @@
 package org.orgname.app;
 
+import org.orgname.app.database.entity.DateEntity;
+import org.orgname.app.database.manager.DateEntityManager;
 import org.orgname.app.ui.CustomTableForm;
 import org.orgname.app.ui.ServiceTableForm;
 import org.orgname.app.util.BaseForm;
@@ -8,6 +10,9 @@ import org.orgname.app.util.MysqlDatabase;
 
 import javax.swing.*;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Application
 {
@@ -27,7 +32,21 @@ public class Application
         initDatabase();
         initUi();
 
-        new ServiceTableForm();
+        DateEntityManager dateEntityManager = new DateEntityManager(database);
+        String s = "13.01.1999";
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
+        try {
+            DateEntity dateEntity = new DateEntity(format.parse(s));
+            dateEntityManager.add(dateEntity);
+            System.out.println(dateEntity);
+
+            System.out.println(dateEntityManager.getById(3));
+
+        } catch (ParseException | SQLException e) {
+            e.printStackTrace();
+        }
+
+        //new ServiceTableForm();
     }
 
     private void initDatabase()
